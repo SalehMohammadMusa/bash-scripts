cac880b937adfb07d676b650cbdbc2496bd4d62b
SalehMohammadMusa <mohammadsaleh1934@gmail.com>
2026-02-07 10:56:40 +0800

    initial commit: basic zabbix script

---
+++ Zabbix/install_zabbix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#check if the script is running as root if not run ask to run as root
+#check which operating system its running 
+#check if there is already a zabbix installed and active in the server 
+#if it is unistall it 
+#if not then we install the zabbix agent
+#we change the configuraiton 
+#restart the agent running with proper configuraition 
+#we are done with the scripts
